86e85557ab5f258a5b04c48bad940fa9f50306e9
theLivingSofa <ayushagrawal10242@gmail.com>
2025-04-05 16:56:52 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import PrivateRoute from './components/PrivateRoute';
+import { BrowserRouter } from 'react-router-dom';
 
 
 createRoot(document.getElementById('root')).render(
+  
   <StrictMode>
+    <BrowserRouter>
       <PrivateRoute>
         <App />
       </PrivateRoute>
+      </BrowserRouter>
   </StrictMode>
 );
 
